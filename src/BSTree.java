36cee2fadb72cc794fcd1fcc97232972502a1abe
Liam McCarthy <lamccart@ucsd.edu>
2018-11-16 23:50:24 -0800

    Finished BSTree - Iter

--- src/BSTree.java
+++ src/BSTree.java
@@ -14,95 +14,179 @@ public class BSTree<T extends Comparable<? super T>> implements Iterable {
         BSTNode right;
 
         public BSTNode(BSTNode left, BSTNode right, LinkedList<T> dataList, T key) {
-            //TODO
+            this.left = left;
+            this.right = right;
+            this.dataList = dataList;
+            this.key = key;
         }
 
         public BSTNode(BSTNode left, BSTNode right, T key) {
-            //TODO
+            this.left = left;
+            this.right = right;
+            this.key = key;
+            this.dataList = new LinkedList<>();
         }
 
         public T getKey() {
-            //TODO
-            return null;
+            return key;
         }
 
         public BSTNode getLeft() {
-            //TODO
-            return null;
+            return left;
         }
 
         public BSTNode getRight() {
-            //TODO
-            return null;
+            return right;
         }
 
         public LinkedList<T> getDataList() {
-            //TODO
-            return null;
+            return dataList;
         }
 
-        public void setleft(BSTNode newleft) {
-            //TODO
+        public void setLeft(BSTNode newleft) {
+            left = newleft;
         }
 
-        public void setright(BSTNode newright) {
-            //TODO
+        public void setRight(BSTNode newright) {
+            right = newright;
         }
 
         public void setDataList(LinkedList<T> newData) {
-            //TODO
+            dataList = newData;
         }
 
         public void addNewInfo(T data) {
-            //TODO
+            dataList.add(data);
         }
 
         public boolean removeInfo(T data) {
-            //TODO
-            return false;
+            if(dataList.contains(data)) {
+                dataList.remove(data);
+                return true;
+            }else{
+                return false;
+            }
         }
     }
 
     public BSTree() {
-        //TODO
+        root = null;
+        nelems = 0;
     }
 
     public BSTNode getRoot() {
-        //TODO
-        return null;
+        if(root != null){
+            return root;
+        }else{
+            return null;
+        }
     }
 
     public int getSize() {
-        //TODO
-        return 0;
+        return nelems;
     }
 
-    public void insert(T key) {
-        //TODO
+    public void insert(T key) throws NullPointerException{
+        if(key == null){
+            throw new NullPointerException();
+        }else{
+            BSTNode node = new BSTNode(null, null, key);
+            if(root == null){
+                root = node;
+            }else{
+                BSTNode currentNode = root;
+                while (currentNode != null){
+                    if ( node.getKey().compareTo(currentNode.getKey()) < 0) {
+                        if (currentNode.getLeft() == null){
+                            currentNode.setLeft(node);
+                            currentNode = null;
+                        }else{
+                            currentNode = currentNode.getLeft();
+                        }
+                    }else{
+                        if (currentNode.getRight() == null){
+                            currentNode.setRight(node);
+                            currentNode = null;
+                        }else{
+                            currentNode = currentNode.getRight();
+                        }
+                    }
+                }
+            }
+            node.setRight(null);
+            node.setLeft(null);
+        }
     }
 
-    public boolean findKey(T key) {
-        //TODO
-        return false;
+    private BSTNode traverseTree(BSTNode currentNode, T key){
+        if (currentNode == null || key.equals(currentNode.getKey())){
+            return currentNode;
+        } else if (key.compareTo(currentNode.getKey()) < 0) {
+            return traverseTree(currentNode.getLeft(), key);
+        } else {
+            return traverseTree(currentNode.getRight(), key);
+        }
     }
 
-    public void insertData(T key, T data) {
-        //TODO;
+    public boolean findKey(T key) throws NullPointerException{
+        if(key == null){
+            throw new NullPointerException();
+        }else{
+            BSTNode currentNode = root;
+            return traverseTree(currentNode, key) != null;
+        }
     }
 
-    public LinkedList<T> findDataList(T key) {
-        //TODO
-        return null;
+    public void insertData(T key, T data) throws NullPointerException, IllegalArgumentException{
+        if(key == null || data == null){
+            throw new NullPointerException();
+        }else if(!findKey(key)){
+            throw new IllegalArgumentException();
+        }else{
+            BSTNode currentNode = root;
+            BSTNode foundNode = traverseTree(currentNode,key);
+            if(foundNode != null){
+                foundNode.addNewInfo(data);
+            }
+        }
+    }
+
+    public LinkedList<T> findDataList(T key) throws NullPointerException, IllegalArgumentException{
+        if(key == null){
+            throw new NullPointerException();
+        }else{
+            BSTNode currentNode = root;
+            BSTNode foundNode = traverseTree(currentNode, key);
+            if(foundNode != null){
+                return foundNode.getDataList();
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private int findNodeHeight(BSTNode node){
+        if(node == null) {
+            return -1;
+        }
+        return Math.max(findNodeHeight(node.getLeft()), findNodeHeight(node.getRight()))+1;
     }
 
     public int findHeight() {
-        //TODO
-        return 0;
+        return findNodeHeight(root);
+    }
+
+    private int getLeafCount(BSTNode node){
+        if (node == null)
+            return 0;
+        if (node.getLeft() == null && node.getRight() == null)
+            return 1;
+        else
+            return getLeafCount(node.left) + getLeafCount(node.right);
     }
 
     public int leafCount() {
-        //TODO
-        return 0;
+        return getLeafCount(root);
     }
 
     //******************ITERATOR STARTS HERE!!!********************************
